7d8237e8b347a314b4310fc6adc98ab4d9b9092c
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2025-06-06 15:06:09 -0400

    Solved 16 faster...

--- problems/16.3-sum-closest.ts
+++ problems/16.3-sum-closest.ts
@@ -1,51 +1,29 @@
 function threeSumClosest(nums: number[], target: number): number {
     nums.sort((a, b) => a - b);
 
+    let closestResult = 0;
     let minDifference = Infinity;
 
     for (let i = 0; i < nums.length; i++) {
-        while (nums[i] === nums[i - 1]) {
-            i++;
-        }
-
         let j = i + 1;
         let k = nums.length - 1;
 
-        function moveJ() {
-            j++;
-
-            while (nums[j] === nums[j - 1] && j < k) {
-                j++;
-            }
-        }
-
-        function moveK() {
-            k--;
-
-            while (nums[k] === nums[k + 1] && j < k) {
-                k--;
-            }
-        }
-
         while (j < k) {
             const sum = nums[i] + nums[j] + nums[k];
-            const difference = sum - target;
+            const difference = Math.abs(sum - target);
 
-            if (Math.abs(difference) < Math.abs(minDifference)) {
+            if (difference < minDifference) {
                 minDifference = difference;
+                closestResult = sum;
             }
 
-            if (difference === 0) {
-                return target + difference;
-            } else if (difference > 0) {
-                moveK();
-            } else if (difference < 0) {
-                moveJ();
+            if (sum < target) {
+                j++;
+            } else {
+                k--;
             }
         }
     }
 
-    return target + minDifference;
+    return closestResult;
 }
-
-console.log(threeSumClosest([-1, 2, 1, -4], 1));
